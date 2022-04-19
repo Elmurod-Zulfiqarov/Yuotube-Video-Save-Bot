86e5eb0a2fb6af139f283d690731cf8978f2eaca
elmurod <elmurodzulfiqarov@gmail.com>
2022-04-19 12:31:03 +0500

    fixed some bugs

--- data/config.py
+++ data/config.py
@@ -9,6 +9,3 @@
 # ADMINS = env.list("ADMINS")  # adminlar ro'yxati
 # IP = env.str("ip")  # Xosting ip manzili
 
-ADMINS=[846140804]
-BOT_TOKEN='5263785700:AAEPVPxiOOOHcdBMuNGM9CskITT3E5tqsfA'
-CHANNELS = [-1001734953411]
